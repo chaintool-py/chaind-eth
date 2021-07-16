c35957275c6533c3244b3a41bb310bea57f06315
nolash <dev@holbrook.no>
2021-07-16 21:48:30 +0200

    add filter test

--- tests/chaind_eth_base.py
+++ tests/chaind_eth_base.py
@@ -4,11 +4,13 @@ import unittest
 # external imports
 from chainsyncer.unittest.db import ChainSyncerDb
 from chainqueue.unittest.db import ChainQueueDb
+from chainlib.eth.unittest.ethtester import EthTesterCase
 
 
-class TestBase(unittest.TestCase):
+class TestBase(EthTesterCase):
 
     def setUp(self):
+        super(TestBase, self).setUp()
         self.db_chainsyncer = ChainSyncerDb()
         self.session_chainsyncer = self.db_chainsyncer.bind_session()
 
@@ -20,3 +22,4 @@ class TestBase(unittest.TestCase):
         self.db_chainsyncer.release_session(self.session_chainsyncer)
         self.session_chainqueue.commit()
         self.db_chainqueue.release_session(self.session_chainqueue)
+        super(TestBase, self).tearDown()
